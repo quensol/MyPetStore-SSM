24709c3d7fa7fe1091ce221d88b68b345cfc150e
Tao-Yuan7 <706842215@qq.com>
2024-03-24 11:44:56 +0800

    Initial Submit

---
+++ src/test/java/org/csu/petstore/MyPetStoreSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetStoreSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
